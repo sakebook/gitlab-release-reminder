86443e6430a5e5418e3135f795eb4ad028f5373b
sakebook <sakebook@gmail.com>
2019-03-19 18:43:29 +0900

    Add library settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    java
     kotlin("jvm") version "1.3.20"
+    id("com.google.cloud.tools.jib") version "1.0.2"
 }
 
 group = "com.github.sakebook"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven("https://kotlin.bintray.com/kotlinx")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.kittinunf.fuel", "fuel", "2.0.1")
+    implementation("com.github.kittinunf.fuel", "fuel-kotlinx-serialization", "2.0.1")
+    testImplementation("junit", "junit", "4.12")
 }
 
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
